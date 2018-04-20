280c489a6fd1d975a85ed107df1447bb6c956f88
kotpsot <zygmunt.justyna@gmail.com>
2018-04-20 20:46:22 +0100

    Add 2nd array examole.

--- arrays.cs
+++ arrays.cs
@@ -1,13 +1,13 @@
-using System;
+// using System;
 
-namespace MyApp 
-{
-    class Program 
-    {
-        static void Main(string[] args){
-            string[] names = new string[1];
-            names[0] = "John Doe";
-            Console.WriteLine(names[names.Length-1]);
-        }
-    }
-}
+// namespace MyApp 
+// {
+//     class Program 
+//     {
+//         static void Main(string[] args){
+//             string[] names = new string[1];
+//             names[0] = "John Doe";
+//             Console.WriteLine(names[names.Length-1]);
+//         }
+//     }
+// }
