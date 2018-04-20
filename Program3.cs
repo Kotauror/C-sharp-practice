6a893c3feb6afa188a5c4f2c30bf0498bb9c46a0
kotpsot <zygmunt.justyna@gmail.com>
2018-04-20 16:31:16 +0100

    Refactor if statement.

--- Program3.cs
+++ Program3.cs
@@ -1,24 +1,24 @@
 using System;
 
-namespace ConsoleApplication1
+namespace App 
 {
-    class Program
+    class Program 
     {
+
         static void Main(string[] args)
         {
             int number;
 
-            Console.WriteLine("Please enter a number between 0 and 10:");
+            Console.WriteLine("Write a number from 0 to 10");
             number = int.Parse(Console.ReadLine());
 
-            if(number > 10)
-                Console.WriteLine("Hey! The number should be 10 or less!");
-            else
+            if(number > 10) 
+                Console.WriteLine("It should be smaller than 10");
+            else 
                 if(number < 0)
-                    Console.WriteLine("Hey! The number should be 0 or more!");
-                else
-                    Console.WriteLine("Good job!");
-
+                    Console.WriteLine("It should be bigger than 0");
+                else 
+                    Console.WriteLine("Good job");
             Console.ReadLine();
         }
     }
