7c3045a49fadf2f4fd850a389c41462b7ec90283
kotpsot <zygmunt.justyna@gmail.com>
2018-04-20 20:43:48 +0100

    Add array example.

--- reference1.cs
+++ reference1.cs
@@ -1,23 +1,22 @@
-using System;
+// using System;
 
-namespace MyApp
-{
-    class Program 
-    {
-        static void Main(string[] args)
-        {
-           int number = 20;
-           AddFive(number);
-           Console.WriteLine(number);
-           Console.ReadKey();
+// namespace MyApp
+// {
+//     class Program 
+//     {
+//         static void Main(string[] args)
+//         {
+//            int number = 20;
+//            AddFive(ref number);
+//            Console.WriteLine(number);
 
-        }
+//         }
 
-        static void AddFive(int number)
-        {
-            number = number + 5;
-        }
-    }
-}
+//         static void AddFive(ref int number)
+//         {
+//             number = number + 5;
+//         }
+//     }
+// }
 
-// by value - we dont change the initial number = 20, only use a copy of its value.
+// // by value - we dont change the initial number = 20, only use a copy of its value.
