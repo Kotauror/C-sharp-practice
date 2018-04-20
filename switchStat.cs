a80d84e8e0a24d6453d14f66a12bc6b684faf3e3
kotpsot <zygmunt.justyna@gmail.com>
2018-04-20 17:46:38 +0100

    Add another switch statement and a while loop.

--- switchStat.cs
+++ switchStat.cs
@@ -1,31 +1,31 @@
-using System;
+// using System;
 
-namespace myApp
-{
-    class Program 
-     {
+// namespace myApp
+// {
+//     class Program 
+//      {
 
-         static void Main(string[] args)
-         {
-             int number;
+//          static void Main(string[] args)
+//          {
+//              int number;
 
-             Console.WriteLine("Please write a number");
-             number = int.Parse(Console.ReadLine());
+//              Console.WriteLine("Please write a number");
+//              number = int.Parse(Console.ReadLine());
 
-            switch(number)
-            {
-                case 0:
-                    Console.WriteLine("The number is zero!");
-                    break;
-                case 1:
-                    Console.WriteLine("The number is one!");
-                    break;
-                default: 
-                    Console.WriteLine("The number not one or zero");
-                    break;
-            }
-         }
+//             switch(number)
+//             {
+//                 case 0:
+//                     Console.WriteLine("The number is zero!");
+//                     break;
+//                 case 1:
+//                     Console.WriteLine("The number is one!");
+//                     break;
+//                 default: 
+//                     Console.WriteLine("The number not one or zero");
+//                     break;
+//             }
+//          }
 
-     }
+//      }
     
-}
+// }
---
+++ switchStat2.cs
@@ -0,0 +1,31 @@
+// using System;
+
+// namespace myApp
+// {
+//     class Program 
+//      {
+//          static void Main(string[] args)
+//          {
+//              string answer;
+
+//              Console.WriteLine("Do you like me? /yes/no/maybe");
+//              answer = Console.ReadLine();
+
+//             switch(answer.ToLower())
+//             {
+//                 case "yes":
+//                     Console.WriteLine("Nice!");
+//                     break;
+//                 case "maybe":
+//                     Console.WriteLine("Maybe lol");
+//                     break;
+//                 case "no":
+//                     Console.WriteLine("Well, you should change your mind then");
+//                     break;
+//                 default: 
+//                     Console.WriteLine("write yes/no/maybe");
+//                     break;
+//             }
+//          }
+//      }  
+// }
