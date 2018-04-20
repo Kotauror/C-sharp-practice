8e780db57a7ef7971094b60464be71d50f57af09
kotpsot <zygmunt.justyna@gmail.com>
2018-04-20 18:13:35 +0100

    Add for loop.

---
+++ forloop.cs
@@ -0,0 +1,13 @@
+using System; 
+
+namespace MyApp 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++) 
+                Console.WriteLine(i);
+        }
+    }
+}
